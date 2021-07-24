b9a8e42f8e006f02ecb0e612b0f8821dd5727929
andyjzhong <andyjzhong@gmail.com>
2021-07-24 10:51:56 -0400

    did it by myself

--- 11-Arrays-Bankist/starter/challenge.js
+++ 11-Arrays-Bankist/starter/challenge.js
@@ -1,27 +1,33 @@
-dogsJulia = [3, 5, 2, 12, 7];
-dogsKate = [4, 1, 15, 8, 3];
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
 
-let dogsJuliaReal = [...dogsJulia]
+const dogsJulia2 = [3, 5, 2, 12, 7];
+const dogsKate2 = [4, 1, 15, 8, 3];
 
-const removeCats = (dogsJulia) => {
-  console.log("BEFORE:::", dogsJuliaReal);
-  dogsJuliaReal.shift()
-  dogsJuliaReal.splice(-2, 2)
-  console.log("AFTER:::", dogsJuliaReal);
+
+const checkDogs = (dogsJulia, dogsKate) => {
+  let dogsJuliaReal = [...dogsJulia]
+
+  const removeCats = (dogsJulia) => {
+    // console.log("BEFORE:::", dogsJuliaReal);
+    dogsJuliaReal.shift()
+    dogsJuliaReal.splice(-2, 2)
+    // console.log("AFTER:::", dogsJuliaReal);
+  }
+
+  removeCats([3, 5, 2, 12, 7])
+
+  const allDogs = dogsJuliaReal.concat(dogsKate);
+  // console.log(allDogs);
+
+  allDogs.forEach(function(dog, i) {
+    if (dog >= 3) {
+      // console.log("Adult");
+      console.log(`Dog number ${i + 1} is an adult`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy`);
+    }
+  });
 }
 
-removeCats([3, 5, 2, 12, 7])
-
-const allDogs = dogsJuliaReal.concat(dogsKate);
-console.log(allDogs);
-
-//
-//
-// const checkDogs (dogsJulia, dogsKate) => {
-//
-// }
-//
-// if dogs age >= 3 ? adult : puppy
-//
-//
-// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs(dogsJulia, dogsKate);
